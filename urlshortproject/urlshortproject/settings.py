8a24ba73b0e8acae3ebb22e94e8b3d983f33a11f
Korsah Solomon Kwaku <98711919+Korsah23@users.noreply.github.com>
2022-11-27 11:21:10 +0000

    Update settings.py
    removed secret Key

--- urlshortproject/urlshortproject/settings.py
+++ urlshortproject/urlshortproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wubfe!+!x&)3k9%vl9esa9re&spw@unl8pq%#4n0x0b&xk&5ea'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
